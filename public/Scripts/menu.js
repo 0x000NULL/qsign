ba9b39c7cb14a4d9e89d287dbcabaa2cb8aa3e73
danstyx <64683826+danstyx@users.noreply.github.com>
2020-05-08 10:18:56 -0700

    Massive Update
    Added window.onload function that prints a form inside the #main id. From there, you can select Create or Sign. Once you choose one, it will replace the node's children with new ones with another .onclick method. Parameter entries are done, but return values are not.

--- public/Scripts/menu.js
+++ public/Scripts/menu.js
@@ -1,19 +1,185 @@
-// Main Menu
+window.onload = function() {
+	var x = document.getElementById("menu");
+	var createform = document.createElement('form');
+	x.appendChild(createform);
+	
+	var bCreate = document.createElement('a');
+	bCreate.innerHTML = "Create";
+	createform.appendChild(bCreate);
+	bCreate.addEventListener("click", bCreate);
+	
+	bCreate.onclick = function() {
+		var nameLabel = document.createElement('label'); // Adds variables for necessary creation parameters.
+		nameLabel.innerHTML = "Your Name:";
+		
+		var nameInput = document.createElement('input');
+		nameInput.setAttribute("type", "text");
+		nameInput.setAttribute("name", "nameInput");
+		nameInput.setAttribute("id", "nameInput");
+		
+		var gradeLabel = document.createElement('label');
+		gradeLabel.innerHTML = "Your Grade:";
+		
+		var gradeInput = document.createElement('input');
+		gradeInput.setAttribute("type", "text");
+		gradeInput.setAttribute("name", "gradeInput");
+		gradeInput.setAttribute("id", "gradeInput");
+		
+		var back = document.createElement('button');
+		back.innerHTML = "Back";
+		
+		back.onclick = function() {
+			createform.remove();
+			redo();
+		}
+		
+		var cSubmit = document.createElement('button');
+		cSubmit.innerHTML = "Submit";
+		
+		cSubmit.onclick = function() {
+			var nameData = document.getElementById("nameInput");
+			var gradeData = document.getElementById("gradeInput");
+			return nameData + " " + gradeData;
+		}
+		
+		createform.replaceChild(nameLabel, bCreate); // Removes all previous form choices.
+		createform.replaceChild(nameInput, orTxt);
+		createform.replaceChild(gradeLabel, bSign);
+		createform.appendChild(gradeInput);
+		createform.appendChild(back);
+		createform.appendChild(cSubmit);
+	}
 
-var x = document.getElementById("menu");
-var createform = document.createElement('form');
-x.appendChild(createform);
+	var orTxt = document.createElement('p');
+	orTxt.innerHTML = "- or -";
+	createform.appendChild(orTxt);
 
-var bCreate = document.createElement('button');
-bCreate.innerHTML = "Create";
-bCreate.onclick = "window.location.href='create'";
-createform.appendChild(bCreate);
+	var bSign = document.createElement('a');
+	bSign.innerHTML = "Sign";
+	createform.appendChild(bSign);
+	bSign.addEventListener("click", bSign);
+	
+	bSign.onclick = function() {
+		var signLabel = document.createElement('label'); // Adds variables for necessary creation parameters.
+		signLabel.innerHTML = "Sign Code:";
+		
+		var signCode = document.createElement('input');
+		signCode.setAttribute("type", "text");
+		signCode.setAttribute("name", "signCode");
+		signCode.setAttribute("id", "signCode");
+		
+		var back = document.createElement('button');
+		back.innerHTML = "Back";
+		
+		back.onclick = function() {
+			createform.remove();
+			redo();
+		}
+		
+		var sSubmit = document.createElement('button');
+		sSubmit.innerHTML = "Submit";
+		
+		sSubmit.onclick = function() {
+			var signCode = document.getElementById("signCode");
+			return signCode;
+		}
+		
+		createform.replaceChild(signLabel, bCreate); // Removes all previous form choices.
+		createform.replaceChild(signCode, orTxt);
+		createform.replaceChild(back, bSign);
+		createform.appendChild(sSubmit);
+	}
+}
 
-var orTxt = document.createElement('p');
-orTxt.innerHTML = "- or -";
-createform.appendChild(orTxt);
+function redo() {
+	var x = document.getElementById("menu");
+	var createform = document.createElement('form');
+	x.appendChild(createform);
+	
+	var bCreate = document.createElement('a');
+	bCreate.innerHTML = "Create";
+	createform.appendChild(bCreate);
+	bCreate.addEventListener("click", bCreate);
+	
+	bCreate.onclick = function() {
+		var nameLabel = document.createElement('label'); // Adds variables for necessary creation parameters.
+		nameLabel.innerHTML = "Your Name:";
+		
+		var nameInput = document.createElement('input');
+		nameInput.setAttribute("type", "text");
+		nameInput.setAttribute("name", "nameInput");
+		nameInput.setAttribute("id", "nameInput");
+		
+		var gradeLabel = document.createElement('label');
+		gradeLabel.innerHTML = "Your Grade:";
+		
+		var gradeInput = document.createElement('input');
+		gradeInput.setAttribute("type", "text");
+		gradeInput.setAttribute("name", "gradeInput");
+		gradeInput.setAttribute("id", "gradeInput");
+		
+		var back = document.createElement('button');
+		back.innerHTML = "Back";
+		
+		back.onclick = function() {
+			createform.remove();
+			redo();
+		}
+		
+		var cSubmit = document.createElement('button');
+		cSubmit.innerHTML = "Submit";
+		
+		cSubmit.onclick = function() {
+			var nameData = document.getElementById("nameInput");
+			var gradeData = document.getElementById("gradeInput");
+			console.log(nameData + " " + gradeData);
+		}
+		
+		createform.replaceChild(nameLabel, bCreate); // Removes all previous form choices.
+		createform.replaceChild(nameInput, orTxt);
+		createform.replaceChild(gradeLabel, bSign);
+		createform.appendChild(gradeInput);
+		createform.appendChild(back);
+		createform.appendChild(cSubmit);
+	}
 
-var bJoin = document.createElement('button');
-bJoin.innerHTML = "Join";
-bJoin.onclick = "window.location.href='join'";
-createform.appendChild(bJoin);
+	var orTxt = document.createElement('p');
+	orTxt.innerHTML = "- or -";
+	createform.appendChild(orTxt);
+
+	var bSign = document.createElement('a');
+	bSign.innerHTML = "Sign";
+	createform.appendChild(bSign);
+	bSign.addEventListener("click", bSign);
+	
+	bSign.onclick = function() {
+		var signLabel = document.createElement('label'); // Adds variables for necessary creation parameters.
+		signLabel.innerHTML = "Sign Code:";
+		
+		var signCode = document.createElement('input');
+		signCode.setAttribute("type", "text");
+		signCode.setAttribute("name", "signCode");
+		signCode.setAttribute("id", "signCode");
+		
+		var back = document.createElement('button');
+		back.innerHTML = "Back";
+		
+		back.onclick = function() {
+			createform.remove();
+			redo();
+		}
+		
+		var sSubmit = document.createElement('button');
+		sSubmit.innerHTML = "Submit";
+		
+		sSubmit.onclick = function() {
+			var signCode = document.getElementById("signCode");
+			return signCode;
+		}
+		
+		createform.replaceChild(signLabel, bCreate); // Removes all previous form choices.
+		createform.replaceChild(signCode, orTxt);
+		createform.replaceChild(back, bSign);
+		createform.appendChild(sSubmit);
+	}
+}
